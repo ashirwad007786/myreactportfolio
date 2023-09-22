deb84d937d7f8d26b1ddb72cb9ef8a9458151d51
ashirwad007786 <ashirwadbarnwal15@gmail.com>
2023-09-22 13:53:27 +0530

    Routinsg

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,11 +1,17 @@
 
-import React from "react";
+import React,{useState} from "react";
 import { Button } from "react-bootstrap";
 import Logo from "../Images/Abpng.gif";
 import { Container, Navbar } from "react-bootstrap";
 import NavB from "react-bootstrap/Nav";
+import { Link } from "react-router-dom";
 
 export default function Nav() {
+  const [navbarExpanded, setNavbarExpanded] = useState(false);
+
+  const closeNavbar = () => {
+    setNavbarExpanded(false);
+  };
   return (
     <Navbar expand="lg" className="navbar p-0">
       <Container fluid className="px-1">
@@ -15,13 +21,13 @@ export default function Nav() {
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse className="justify-content-end" id="basic-navbar-nav">
           <NavB className="px-5">
-            <NavB.Link href="/myreactportfolio/">Home</NavB.Link>
-            <NavB.Link href="/myreactportfolio/about">About</NavB.Link>
-            <NavB.Link href="/myreactportfolio/skills">Skills</NavB.Link>
-            <NavB.Link href="/myreactportfolio/contact">Contact</NavB.Link>
+          <NavB.Link as={Link} to="/" onClick={closeNavbar}>Home</NavB.Link>
+            <NavB.Link as={Link} to="/about" onClick={closeNavbar}>About</NavB.Link>
+            <NavB.Link as={Link} to="/skills" onClick={closeNavbar}>Skills</NavB.Link>
+            <NavB.Link as={Link} to="/contact" onClick={closeNavbar}>Contact</NavB.Link>
             <Button
               className="resume-btn"
-              onClick={() => {window.open("https://drive.google.com/file/d/1po5W7AWphOHMfo4ymGaNMjdYHuDRoHdi/view?usp=sharing");}}>
+              onClick={() => {window.open("https://drive.google.com/file/d/1QsARkuDuBLDUC0btZtCuA_hmd9qHfsEB/view?usp=sharing");}}>
               <span>Resume</span>
             </Button>
           </NavB>
