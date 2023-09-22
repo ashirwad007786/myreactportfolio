f28625f0c1fee6bd85e0a2c991932904c77b359f
ashirwad007786 <ashirwadbarnwal15@gmail.com>
2023-09-22 13:45:02 +0530

    Routinsg

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -9,16 +9,16 @@ export default function Nav() {
   return (
     <Navbar expand="lg" className="navbar p-0">
       <Container fluid className="px-1">
-        <Navbar.Brand href="/">
+        <Navbar.Brand href="/myreactportfolio/">
           <img className="logo" src={Logo} alt="logo" />
         </Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse className="justify-content-end" id="basic-navbar-nav">
           <NavB className="px-5">
-            <NavB.Link href="/">Home</NavB.Link>
-            <NavB.Link href="/about">About</NavB.Link>
-            <NavB.Link href="/skills">Skills</NavB.Link>
-            <NavB.Link href="/contact">Contact</NavB.Link>
+            <NavB.Link href="/myreactportfolio/">Home</NavB.Link>
+            <NavB.Link href="/myreactportfolio/about">About</NavB.Link>
+            <NavB.Link href="/myreactportfolio/skills">Skills</NavB.Link>
+            <NavB.Link href="/myreactportfolio/contact">Contact</NavB.Link>
             <Button
               className="resume-btn"
               onClick={() => {window.open("https://drive.google.com/file/d/1po5W7AWphOHMfo4ymGaNMjdYHuDRoHdi/view?usp=sharing");}}>
